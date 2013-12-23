7eb1823576aad953eaae8441f3b38ded43871a0a
jejacks0n <jejacks0n@gmail.com>
2013-12-23 12:41:18 -0700

    adds ability to install assets with the install generator. #8

--- lib/generators/apitome/install/install_generator.rb
+++ lib/generators/apitome/install/install_generator.rb
@@ -5,9 +5,20 @@ module Apitome
 
       desc "Installs the Apitome initializer and markdown file into your application."
 
+      class_option :assets, type: :boolean,
+                   aliases: "-a",
+                   default: false,
+                   desc:    "Install the javascript and stylesheet assets to /public"
+
       def copy_initializers
         copy_file "templates/initializer.rb", "config/initializers/apitome.rb"
         copy_file "templates/api.md", "doc/api.md"
+      end
+
+      def copy_assets
+        return unless options[:assets]
+        copy_file "../../../../app/assets/javascripts/apitome/bundle.js", "public/assets/apitome/application.js"
+        copy_file "../../../../app/assets/stylesheets/apitome/bundle.css", "public/assets/apitome/application.css"
       end
 
       def display_readme
