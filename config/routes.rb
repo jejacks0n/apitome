d43882e79d68e5360568cd4f0b2866c621cee7c6
Jeremy Jackson <jejacks0n@gmail.com>
2017-07-06 10:38:20 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Apitome::Engine.routes.draw do
   root to: "docs#index"
-  get "/simulate/*path", to: "docs#simulate"
+  get "/simulate/*path", to: "docs#simulate", as: :simulated
   get "/*path", to: "docs#show" unless Apitome.configuration.single_page
 end
