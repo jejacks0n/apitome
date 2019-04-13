2eaebc87d9b6124280199b85517a609ad58b7bd4
jejacks0n <jejacks0n@gmail.com>
2019-04-13 12:10:28 -0600

    General cleanup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
-require "codeclimate-test-reporter"
-
-# require "simplecov"
-# SimpleCov.start
-
 ENV["RAILS_ENV"] ||= "test"
 ENV["RAILS_ROOT"] = File.expand_path("../dummy", __FILE__)
-require File.expand_path("../dummy/config/environment", __FILE__)
+require_relative "dummy/config/environment"
 
 require "apitome"
 
@@ -13,14 +8,27 @@ require "rspec/rails"
 require "capybara/rails"
 require "aruba/api"
 
-require "fileutils"
-require "ostruct"
+# require "fileutils"
+# require "ostruct"
 
-Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
+# Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
+
+
+def register_driver(name, args = [], opts = {})
+  Capybara.register_driver(name) do |app|
+    options = { args: args + ["window-size=1440,1080"] }
+    options[:binary] = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: options.compact)
+    Capybara::Selenium::Driver.new(app, { browser: :chrome, desired_capabilities: capabilities }.merge(opts))
+  end
+end
+
+register_driver(:chrome)
+register_driver(:chrome_headless, %w[headless disable-gpu no-sandbox disable-dev-shm-usage])
 
 RSpec.configure do |config|
   config.order = "random"
-  config.include Rack::Test::Methods
+  # config.include Rack::Test::Methods
   config.include Aruba::Api
 
   config.before(:each, shell: true) do
@@ -28,15 +36,7 @@ RSpec.configure do |config|
     setup_aruba
   end
 
-  config.after(:each, shell: true) do
-    restore_env
-    setup_aruba
-  end
-
   config.before(:each, browser: true) do
-    Capybara.current_driver = Capybara.javascript_driver
-    Capybara.register_driver :selenium do |app|
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
-    end
+    Capybara.default_driver = Capybara.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "chrome_headless").to_sym
   end
 end
