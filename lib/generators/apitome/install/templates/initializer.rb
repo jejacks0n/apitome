777c0a5d0838f5f5168d548706d27039e008ba45
Guo Lijun <lijungo@gmail.com>
2019-04-22 14:35:16 +0800

    fixed url_formatter for unicode support

--- lib/generators/apitome/install/templates/initializer.rb
+++ lib/generators/apitome/install/templates/initializer.rb
@@ -46,7 +46,7 @@ Apitome.configure do |config|
 
   # You can specify how urls are formatted using a Proc or other callable object.  Your proc will be called with a
   # resource name or link, giving you the opportunity to modify it as necessary for in the documentation url.
-  config.url_formatter = -> (str) { str.gsub(/\.json$/, '').underscore.gsub(/[^0-9a-z\:]+/i, '-') }
+  config.url_formatter = -> (str) { str.gsub(/\.json$/, '').underscore.gsub(/[^[:word:]]/, '-') }
 
   # You can setup the docs to be loaded from a remote URL if they are not available in the application environment. This
   # URL is used as the base location and should be set to where the readme is located. If left nil, local is assumed.
